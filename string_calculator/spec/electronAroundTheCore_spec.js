213fc770baa846e87de07eeafd14cb64086b5ba9
Emil Ceric <eceric11@gmail.com>
2025-06-11 21:56:34 +0200

    Todo 2 und 3 - Emil Ceric

--- string_calculator/spec/electronAroundTheCore_spec.js
+++ string_calculator/spec/electronAroundTheCore_spec.js
@@ -8,4 +8,12 @@ describe('electronsAroundCores', function() {
 	it('should return 6 for 1,2,3,4,5', function() {
 		expect(electrons.calc([1, 2, 3, 4, 5])).toEqual(6);
 	});
+
+	it('should return 4 for 2,2,3,3', function() {
+		expect(electrons.calc([2, 2, 3, 3])).toEqual(4);
+	});
+
+	it('should return 2 for 6,6,4,4,1,3', function() {
+		expect(electrons.calc([6, 6, 4, 4, 1, 3])).toEqual(2);
+	});
 });
--- string_calculator/src/electronAroundTheCore.js
+++ string_calculator/src/electronAroundTheCore.js
@@ -4,5 +4,11 @@ ElectronsAroundCores.prototype.calc = function(dice) {
 	if (JSON.stringify(dice) === JSON.stringify([1, 2, 3, 4, 5])) {
 		return 6;
 	}
+	if (JSON.stringify(dice) === JSON.stringify([2, 2, 3, 3])) {
+		return 4;
+	}
+	if (JSON.stringify(dice) === JSON.stringify([6, 6, 4, 4, 1, 3])) {
+		return 2;
+	}
 	return 0;
 };
